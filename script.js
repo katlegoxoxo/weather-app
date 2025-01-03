3928175e048e4214abeb8c519b10e7211f5865ce
katlegoxoxo <kmakete421@gmail.com>
2025-01-03 22:42:56 +0200

    Add weather data API endpoints and DOM element bindings

---
+++ script.js
@@ -0,0 +1,17 @@
+const userLocation = document.getElementById("userLocation"),
+    converter = document.getElementById("converter"),
+    weatherIcon = document.querySelector(".weatherIcon"),
+    temperature = document.querySelector(".temperature"),
+    feelsLike = document.querySelector(".feelsLike"),
+    description = document.querySelector(".description"),
+    date = document.querySelector(".date"),
+    city = document.querySelector(".city"),
+
+    HValue = document.getElementById("HValue"),
+    WValue = document.getElementById("WValue"),
+    SRValue = document.getElementById("SRValue"),
+    SSValue = document.getElementById("SSValue"),
+    CValue = document.getElementById("CValue"),
+    UVValue = document.getElementById("UVValue"),
+    PValue = document.getElementById("PValue"),
+    Forcast= document.querySelector(".Forcast");
