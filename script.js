045c93baa527f79349a31ec41572b625e9028b1b
katlegoxoxo <kmakete421@gmail.com>
2025-01-04 20:40:07 +0200

    Add user location fetch function and update weather API endpoints

--- script.js
+++ script.js
@@ -14,4 +14,20 @@ const userLocation = document.getElementById("userLocation"),
     CValue = document.getElementById("CValue"),
     UVValue = document.getElementById("UVValue"),
     PValue = document.getElementById("PValue"),
-    Forcast= document.querySelector(".Forcast");
+    Forcast = document.querySelector(".Forcast");
+
+WEATHER_API_ENDPOINT = `https://api.openweathermap.org/data/2.5/weather?appid=781a520a555e19715c7c1c576386528b&q=`;
+WEATHER_DATA_ENDPOINT = `https://api.openweathermap.org/data/3.0/onecall?appid=781a520a555e19715c7c1c576386528b&exclude=minutely&units=metric&`;
+
+
+function findUserLocation() {
+    fetch(WEATHER_API_ENDPOINT + userLocation.value)
+        .then((response) => response.json())
+        .then((data) => {
+            if (data.cod != '' && data.cod !=200){
+              alert(data.message);  
+              return;
+            }
+            console.log(data.coord.lon,data.coord.lat);
+        });
+}
