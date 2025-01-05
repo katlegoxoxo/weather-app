833f139f479a6e4ab6a267658c173c8d114aebb6
katlegoxoxo <kmakete421@gmail.com>
2025-01-05 21:12:19 +0200

    Update findUserLocation function: Add formatting for date, time, and weather details

--- script.js
+++ script.js
@@ -29,15 +29,55 @@ function findUserLocation() {
                 return;
             }
             console.log(data);
-            city.innerHTML=data.name + " ," + data.sys.country;
-            weatherIcon.style.background= `url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)`
+            city.innerHTML = data.name + " ," + data.sys.country;
+            weatherIcon.style.background = `url(https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png)`
             fetch(WEATHER_DATA_ENDPOINT + `lon=${data.coord.lon}&lat=${data.coord.lat}`)
                 .then((response) => response.json())
                 .then((data) => {
                     console.log(data);
-                    temperature.innerHTML=data.current.temp;
-                    feelsLike.innerHTML="Feels Like " + data.current.feels_like;
-                description.innerHTML= `<i class= "fa-brands fa-cloudversify"></i> &nbsp; `+ data.current.weather[0].description
+                    temperature.innerHTML = data.current.temp;
+                    feelsLike.innerHTML = "Feels Like " + data.current.feels_like;
+                    description.innerHTML = `<i class= "fa-brands fa-cloudversify"></i> &nbsp; ` + data.current.weather[0].description;
+
+                    const options = {
+                        weekday: "long", // e.g., "Monday"
+                        month: "long",   // e.g., "January"
+                        day: "numeric",  // e.g., "15"
+                        hour: "numeric", // e.g., "14"
+                        minute: "numeric", // e.g., "30"
+                        hour12: false,   // 24-hour format
+                    };
+                    
+                    date.innerHTML = getLongFormateDateTime(data.current.dt, data.timezone_offset, options);
+                    
+                    HValue.innerHTML = Math.round(data.current.humidity) + "<span>%</span>";
+                    WValue.innerHTML = Math.round(data.current.wind_speed) + "<span>m/s</span>";
+
+                    const options1 = {
+                        hour: "numeric",
+                        minute: "numeric",
+                        hour12: false,
+                    };
+                    SRValue.innerHTML = getLongFormateDateTime(data.current.sunrise, data.timezone_offset, options1);
+                    SSValue.innerHTML = getLongFormateDateTime(data.current.sunset, data.timezone_offset, options1);
+
+                    CValue.innerHTML = data.current.clouds + "<span>%</span>";
+                    UVValue.innerHTML = data.current.uvi;
+                    PValue.innerHTML = data.current.pressure + "<span>hPa</span>";
+
+
+
+
                 });
         });
+}
+
+
+function formatUnixTime(dtValue, offset, options = {}) {
+    const date = new Date((dtValue + offset) * 1000);
+    return date.toLocaleTimeString([], { timeZone: "UTC", ...options }); // Use toLocaleTimeString for time
+}
+
+function getLongFormateDateTime(dtValue, offset, options) {
+    return formatUnixTime(dtValue, offset, options);
 }
